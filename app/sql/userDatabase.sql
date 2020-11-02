bfdeef2fd2d143e025eb3a4e4d784a9496b742ec
Raze <proriderless@users.noreply.github.com>
2020-11-02 19:46:25 +0800

    Add files via upload

---
+++ app/sql/userDatabase.sql
@@ -0,0 +1,11 @@
+drop database if exists scheduler;
+
+create database scheduler;
+
+use scheduler;
+
+CREATE TABLE if not exists userbase (
+    id integer auto_increment primary key,
+    username varchar(200),
+    passwordHash varchar(1000),
+);
