83f989dda364fd5b1ce448b3122fb5e89c80b3a7
Raze <proriderless@users.noreply.github.com>
2020-11-02 19:44:27 +0800

    Add files via upload

---
+++ app/include/process_login.php
@@ -0,0 +1,59 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+require_once 'model/common.php';
+
+if( trim($_POST['username']) != '' && trim($_POST['password']) != '' ) {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    // Authenticate
+    $dao = new UserDAO();
+
+    // Call AccountDAO's getHashedPassword($username) to hashed_password stored in Account table
+    // See what getHashedPassword($username) returns.
+    //    NULL if username is NOT found in Account table
+    //    hashed_password (String) if username is found in Account table
+
+    $result = $dao->getUserDetails($username);
+
+    //if username does not exist
+    if ($result == null){
+        $msg = "Username doesn't exist!";
+        echo $msg;
+        return;
+    }
+
+    $AuthPass = $result->getHashedPassword();
+    $id = $result->getUserId();
+
+    $status = password_verify($password,$AuthPass);
+
+    if($status === FALSE){
+
+        $msg = "Your username/password could be wrong!";
+        echo $msg;
+        return;
+
+    } else {
+        $_SESSION['username'] = $username;
+        $_SESSION['userid'] = $id;
+        $msg = "Login success!";
+        echo $msg;
+        return;
+    }
+}
+else {
+    // Oh oh... both username/password fields must be filled out!
+
+    // 1) Register Error message as a Session Variable
+    $msg = "Some reason, one input is empty...";
+    echo $msg;
+    return;
+
+}
+
+?>
---
+++ app/include/register_process.php
@@ -0,0 +1,61 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+require_once 'model/common.php';
+
+if( trim($_POST['username']) != '' && trim($_POST['password']) ) {
+
+    // All 3 form input fields are filled out!
+    // Retrieve form input values
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // If passwords do not match:    
+  //  if($password !== $retype_password){
+   //     echo "Password mismatch";
+   //     return;
+  //  }
+
+
+    // Passwords do match so proceed with registration!
+    $dao = new UserDAO();
+
+    $hashedPass = password_hash($password, PASSWORD_DEFAULT);
+
+    //Check for registration!
+    $result = $dao->getUserDetails($username);
+
+    //If user already registered, kick them out!
+    if($result != null) {
+        echo "User is already registered!";
+        return;
+    }
+
+    //If it's a success, register user!
+    $register_result = $dao->register($username, $hashedPass);
+
+
+    // If registration in Account table was SUCCESSFUL
+    if($register_result === TRUE){
+        $msg = "User $username has successfully registered!";
+        echo $msg;
+        return;
+    } else {
+        // Registration failed!
+        $msg = "User $username could not be registered!";
+        echo $msg;
+        return;
+    }
+    
+}
+else {
+    // Not all 2 input fields are filled out!
+    $msg = "All 3 fields must be filled out";
+    echo $msg;
+    return;
+
+}
+
+?>
