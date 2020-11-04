a16e193088aff448a69266ab1b7a38e1da50d005
hxchen-2019 <71025092+hxchen-2019@users.noreply.github.com>
2020-11-04 22:23:22 +0800

    Add files via upload

---
+++ app/sql/generate.sql
@@ -0,0 +1,39 @@
+drop database if exists scheduler;
+
+create database scheduler;
+
+use scheduler;
+
+CREATE TABLE if not exists `event_list` (
+  `event_id` integer auto_increment primary key,
+  `user_id` integer NOT NULL,
+  `date` date NOT NULL,
+  `start_time` TIME NOT NULL,
+  `end_time` TIME NOT NULL,
+  `location` text NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL,
+  `completed` boolean NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET= utf8mb4;
+
+CREATE TABLE if not exists `task_list` (
+  `task_id` integer auto_increment primary key,
+  `user_id` integer NOT NULL,
+  `date` date NOT NULL,
+  `start_time` TIME NOT NULL,
+  `end_time` TIME NOT NULL,
+  `repeatable` text NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE if not exists `unavailable_list` (
+  `unavailable_id` integer auto_increment primary key,
+  `user_id` integer NOT NULL,
+  `date` date NOT NULL,
+  `start_time` TIME NOT NULL,
+  `end_time` TIME NOT NULL,
+  `repeatable` text NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
