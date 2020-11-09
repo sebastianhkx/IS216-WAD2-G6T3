b80467239880f66d6fdc94d111c20c893f8641da
hxchen-2019 <71025092+hxchen-2019@users.noreply.github.com>
2020-11-10 00:30:54 +0800

    Add files via upload

--- app/sql/generate.sql
+++ app/sql/generate.sql
@@ -17,7 +17,7 @@ CREATE TABLE if not exists `event_list` (
 )ENGINE=InnoDB DEFAULT CHARSET= utf8mb4;
 
 CREATE TABLE if not exists `task_list` (
-  `task_id` integer primary key,
+  `task_id` integer auto_increment primary key,
   `user_id` integer NOT NULL,
   `date` date NOT NULL,
   `start_time` TIME NOT NULL,
@@ -28,7 +28,7 @@ CREATE TABLE if not exists `task_list` (
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE if not exists `unavailable_list` (
-  `unavailable_id` integer primary key,
+  `unavailable_id` integer auto_increment primary key,
   `user_id` integer NOT NULL,
   `date` date NOT NULL,
   `start_time` TIME NOT NULL,
@@ -36,21 +36,4 @@ CREATE TABLE if not exists `unavailable_list` (
   `repeatable` text NOT NULL,
   `title` text NOT NULL,
   `description` text NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE if not exists `day_logged_unavailable` (
-    `unavailable_id` integer NOT NULL primary key,
-    `user_id` integer NOT NULL,
-    `day` integer NOT NULL,
-  	`start_time` TIME NOT NULL,
-  	`end_time` TIME NOT NULL
-    
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE if not exists `day_logged_task` (
-    `task_id` integer NOT NULL primary key,
-    `user_id` integer NOT NULL,
-    `day` integer NOT NULL,
-  	`start_time` TIME NOT NULL,
-  	`end_time` TIME NOT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
