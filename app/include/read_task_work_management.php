f9bc1e4c4e6ef5e4027e11fd01280d9d339d8b36
Raze <proriderless@users.noreply.github.com>
2020-11-12 19:42:05 +0800

    Add files via upload

---
+++ app/include/read_task_work_management.php
@@ -0,0 +1,32 @@
+<?php
+
+
+require_once 'common.php';
+//Actual 
+$user_id = $_SESSION['userid'];
+$date = $_POST['date'];
+$currentTime = $_POST['currentTime'];
+
+$dao = new HANDLERDAO();
+$task_list = $dao->get_task_by_date_time($date, $user_id, $currentTime);
+
+$tasks = [];
+foreach( $task_list as $task_object ) {
+    $task = [];
+    $task["task_id"] = $task_object->getTaskID();
+    $task["user_id"] = $task_object->getUser();
+    $task["date"] = $task_object->getDate();
+    $task["start_time"] = $task_object->getStartTime();
+    $task["end_time"] = $task_object->getEndTime();
+    $task["repeatable"] = $task_object->getRepeatable();
+    $task["title"] = $task_object->getTitle();
+    $task["description"] = $task_object->getDescription();
+    $tasks[] = $task;
+}
+// make posts into json and return json data
+$postJSON = json_encode($tasks);
+echo $postJSON;
+
+// }
+
+?>
