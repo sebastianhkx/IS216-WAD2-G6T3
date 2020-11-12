361e05e1bb4a22717f8cbc9d64e9c69ae585570b
hxchen-2019 <71025092+hxchen-2019@users.noreply.github.com>
2020-11-13 00:57:49 +0800

    Add files via upload

---
+++ app/include/model/DAO.php
@@ -0,0 +1,106 @@
+<?php
+
+require_once 'common.php';
+
+class DAO{
+
+    public function get_event_by_date_time($date, $past_time, $time, $user_id){
+
+
+        // STEP 1
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->getConnection();
+        
+      
+        $date_statement = "WHERE user_id = '${user_id}' AND DATE = '${date}' AND start_time between '${past_time}' and '${time}'";
+
+      
+        // STEP 2
+      
+        $sql = "SELECT * from event_list ${date_statement}";
+      
+        $stmt = $conn->prepare($sql);
+      
+        // STEP 3
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      
+        // STEP 4
+        $event_list = [];
+        while( $row = $stmt->fetch() ) {
+          $event_list[] =
+              new EVENT(
+                  $row['event_id'],
+                  $row['user_id'],
+                  $row['date'],
+                  $row['start_time'],
+                  $row['end_time'],
+                  $row['location'],
+                  $row['title'],
+                  $row['description'],
+                  $row['completed']
+                );
+        }
+      
+        // STEP 5
+        $stmt = null;
+        $conn = null;
+      
+        // STEP 6
+        return $event_list;
+      
+      }
+
+
+      public function get_task_by_date_time($date, $past_time, $time, $user_id, $repeatable){
+
+
+        // STEP 1
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->getConnection();
+      
+        
+        $date_statement = "WHERE user_id = '${user_id}' AND DATE = '${date}' AND start_time between '${past_time}' and '${time}' AND repeatable = '${repeatable}'";
+      
+      
+        // STEP 2
+      
+        $sql = "SELECT * from task_list ${month_statment}";
+      
+        $stmt = $conn->prepare($sql);
+      
+        // STEP 3
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      
+        // STEP 4
+        $task_list = [];
+        while( $row = $stmt->fetch() ) {
+          $task_list[] =
+              new TASK(
+                  $row['task_id'],
+                  $row['user_id'],
+                  $row['date'],
+                  $row['start_time'],
+                  $row['end_time'],
+                  $row['repeatable'],
+                  $row['title'],
+                  $row['description']
+                );
+        }
+      
+        // STEP 5
+        $stmt = null;
+        $conn = null;
+      
+        // STEP 6
+        return $task_list;
+      
+      }
+
+
+}
+
+
+
+?>
