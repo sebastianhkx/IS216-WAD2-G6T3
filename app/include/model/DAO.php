496258df56cd050bdaf43d95ef47a265bc0a69f4
hxchen-2019 <71025092+hxchen-2019@users.noreply.github.com>
2020-11-13 17:58:34 +0800

    Update DAO.php

--- app/include/model/DAO.php
+++ app/include/model/DAO.php
@@ -102,6 +102,52 @@ class DAO{
         return $task_list;
       
       }
+    
+    
+        public function get_task_by_time($past_time, $time, $user_id, $repeatable){
+            // STEP 1
+            $connMgr = new ConnectionManager();
+            $conn = $connMgr->getConnection();
+
+
+            // STEP 2
+
+            $sql = "SELECT * from task_list WHERE user_id = :user_id AND start_time between :past_time and :time AND repeatable = :repeatable";
+
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+            $stmt->bindParam(':past_time', $past_time, PDO::PARAM_STR);
+            $stmt->bindParam(':time', $time, PDO::PARAM_STR);
+            $stmt->bindParam(':repeatable', $repeatable, PDO::PARAM_STR);
+
+            // STEP 3
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+            // STEP 4
+            $task_list = [];
+            while( $row = $stmt->fetch() ) {
+              $task_list[] =
+                  new TASK(
+                      $row['task_id'],
+                      $row['user_id'],
+                      $row['date'],
+                      $row['start_time'],
+                      $row['end_time'],
+                      $row['repeatable'],
+                      $row['title'],
+                      $row['description']
+                    );
+            }
+
+            // STEP 5
+            $stmt = null;
+            $conn = null;
+
+            // STEP 6
+            return $task_list;
+      
+      }
 
 
 }
