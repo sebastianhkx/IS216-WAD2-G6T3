c87727c1dd9740656efec2f39ed26a463490ada6
hxchen-2019 <71025092+hxchen-2019@users.noreply.github.com>
2020-11-14 14:48:40 +0800

    Delete connectionv2.php

--- Database/connectionv2.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-    $db="scheduler";
-    $port = 3306;
-	$conn = mysqli_connect($servername, $username, $password,$db, $port);
-?>
