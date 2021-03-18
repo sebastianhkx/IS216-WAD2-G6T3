d6f76abef3f98664294bc3bd7832a4ec7a1cfb46
hxchen-2019 <71025092+hxchen-2019@users.noreply.github.com>
2021-03-18 21:38:29 +0800

    Update generate.sql

--- app/sql/generate.sql
+++ app/sql/generate.sql
@@ -1,57 +1,46 @@
-drop database if exists scheduler;
+drop database if exists PET_APP;
 
-create database scheduler;
+create database PET_APP;
 
-use scheduler;
+use PET_APP;
 
-CREATE TABLE if not exists userbase (
-    id integer auto_increment primary key,
-    username varchar(200),
-    passwordHash varchar(1000),
-    teleHandle varchar(200),
-    fullname varchar(200)
-);
-
-CREATE TABLE if not exists `goal_table` (
-  `goal_id` integer auto_increment primary key,
-  `user_id` integer NOT NULL,
-  `date` date NOT NULL,
-  `description` text NOT NULL
+CREATE TABLE if not exists pets (
+    PETID integer auto_increment primary key,
+    imgPath varchar(2000),
+    Date_and_Time DateTime,
+    BirthDate  date,
+    Pet_name varchar(2000),
+    Pet_breed varchar(2000),
+    Pet_type varchar(2000),
+    Pet_info varchar(2000),
+    pet_status  varchar(2000),
+    Confirm_Adopt  boolean,
+    Confirm_Aban  boolean,
+    AbandonEmail  varchar(200)
 )ENGINE=InnoDB DEFAULT CHARSET= utf8mb4;
 
-CREATE TABLE if not exists `event_list` (
-  `event_id` integer auto_increment primary key,
-  `user_id` integer NOT NULL,
-  `date` date NOT NULL,
-  `start_time` TIME NOT NULL,
-  `end_time` TIME NOT NULL,
-  `location` text NOT NULL,
-  `title` text NOT NULL,
-  `description` text NOT NULL,
-  `completed` boolean NOT NULL
+CREATE TABLE if not exists adopter (
+  AdoptID integer auto_increment primary key,
+  FirstName varchar(2000),
+  LastName varchar(2000),
+  AdoptEmail  varchar(2000),
+  TeleID varchar(2000),
+  Pet_ID varchar(2000),
+  Location  varchar(2000),
+  DateIndicate varchar(2000)
+)ENGINE=InnoDB DEFAULT CHARSET= utf8mb4;
+
+CREATE TABLE if not exists abandoner (
+    AbandonID  integer auto_increment primary key,
+    AbandonEmail  varchar(2000),
+    FirstName DateTime,
+    BirthDate  date,
+    LastName varchar(2000),
+    TeleID  varchar(2000),
+    ChosenDate  varchar(2000) 
 )ENGINE=InnoDB DEFAULT CHARSET= utf8mb4;
 
 
 
 
-CREATE TABLE if not exists `task_list` (
-  `task_id` integer auto_increment primary key,
-  `user_id` integer NOT NULL,
-  `date` date NOT NULL,
-  `start_time` TIME NOT NULL,
-  `end_time` TIME NOT NULL,
-  `repeatable` text NOT NULL,
-  `title` text NOT NULL,
-  `description` text NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE if not exists `unavailable_list` (
-  `unavailable_id` integer auto_increment primary key,
-  `user_id` integer NOT NULL,
-  `date` date NOT NULL,
-  `start_time` TIME NOT NULL,
-  `end_time` TIME NOT NULL,
-  `repeatable` text NOT NULL,
-  `title` text NOT NULL,
-  `description` text NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
